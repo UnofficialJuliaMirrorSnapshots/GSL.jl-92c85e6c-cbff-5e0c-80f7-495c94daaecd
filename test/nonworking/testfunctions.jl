2edcb5a8f561db69ba01fd5eb11f46e9c18321f4
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-04-07 21:38:43 +0000

    Snapshot of branch master taken on 2019-04-07T17:38:42.414-04:00 from "https://github.com/UnofficialJuliaMirror/GSL.jl-92c85e6c-cbff-5e0c-80f7-495c94daaecd"

---
+++ test/nonworking/testfunctions.jl
@@ -0,0 +1,13 @@
+using Compat.Test
+using Compat
+
+#Tests GSL special functions and their error-propagating _e variants
+macro sf_test(sf, args...)
+    @eval begin
+        VERBOSE && print($sf, $args)
+        u = $sf($args...)
+        v = $(Symbol(sf, "_e"))($args...)
+        VERBOSE && println("\t", u, "\t", v)
+        @test isapprox(u, v.val, atol=max(eps(), 2*v.err))
+    end
+end
